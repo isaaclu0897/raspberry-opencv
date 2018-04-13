a313cc133c842aae28dac4b8bb9c5ccf00c8de41
wei <t104306033@ntut.org.tw>
2018-04-14 01:39:46 +0800

    /mechinevision: test the medianBlur & boxPoints but still no good

--- findContours.py
+++ findContours.py
@@ -7,31 +7,84 @@ Created on Mon Apr  9 22:32:55 2018
 """
 
 import cv2
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
+import numpy as np
 
-im = cv2.imread('test.jpg')
-#plt.hist(im.ravel(),256,[0,256]) # 利用直方圖可檢視圖片的明度藉此找到閾值
+def thresh1():
+    im = cv2.imread('test.jpg')
+    imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(imgray,200,255,0)
+    binary,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    hull = [cv2.convexHull(contours[i]) for i in range(len(contours))]
+    cv2.drawContours(im,hull,-1,(255, 0, 0), 2)
+    plt.hist(im.ravel(),256,[0,256])
+    cv2.namedWindow('thresh1', cv2.WINDOW_NORMAL)
+    cv2.imshow('thresh1', thresh)
+def thresh2():
+    im = cv2.imread('test.jpg')
+    imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    GaussianBlur = cv2.GaussianBlur(imgray, (9, 9), 0)
+    ret,thresh = cv2.threshold(GaussianBlur,200,255,0)
+    binary,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    hull = [cv2.convexHull(contours[i]) for i in range(len(contours))]
+    cv2.drawContours(im,hull,-1,(255, 0, 0), 2)
+    plt.hist(GaussianBlur.ravel(),256,[0,256])
+    cv2.namedWindow('thresh2', cv2.WINDOW_NORMAL)
+    cv2.imshow('thresh2', thresh)
+def thresh3():
+    im = cv2.imread('test.jpg')
+    imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    GaussianBlur = cv2.GaussianBlur(imgray, (15, 15), 0)
+#    medianBlur = cv2.medianBlur(GaussianBlur, 9)
+    ret,thresh = cv2.threshold(GaussianBlur,200,255,0)
+    binary,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#    contours = [contours[i] for i in range(len(contours)) if len(contours[i]) > 100]
+    #np.array([len(contours[i]) for i in range(len(contours)) if len(contours[i]) > 100])
+    hull = [cv2.convexHull(contours[i]) for i in range(len(contours))]
+    box = []
+    for i in range(len(contours)):
+        rect = cv2.minAreaRect(contours[i])
+        points = cv2.boxPoints(rect)
+        box.append(np.int0(points))
+#    rect = cv2.minAreaRect(contours[10])
+#    box = cv2.boxPoints(rect)
+#    box = [np.int0(box)]
+    cv2.drawContours(im,box,-1,(255, 0, 0), 2)
+#    plt.hist(medianBlur.ravel(),256,[0,256])
+    cv2.namedWindow('thresh3', cv2.WINDOW_NORMAL)
+    cv2.imshow('thresh3', im)
 
-# 由於opencv只能處裡灰度圖，所以要先用convert成灰階
-imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,200,255,0) # 經測試200為合適閾值
-GaussianBlur = cv2.GaussianBlur(thresh, (9, 9), 0)
-binary,contours,hierarchy = cv2.findContours(GaussianBlur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-hull = [cv2.convexHull(contours[i]) for i in range(len(contours))]
-"""
-cv2.CHAIN_APPROX_NONE,所有的邊界點都會被存儲
-cv2.CHAIN_APPROX_SIMPLE,只存取重要特徵點
-"""
-cv2.drawContours(im,hull,-1,(255, 0, 0), 2)
-"""
-我們可以利用 type和 shape來查看圖片的型態和大小
-print(type(im), im.shape)
-這裡輸出的結果為    # <class 'numpy.ndarray'>, (3200, 2400, 3)
-實在是太大了，因此我們可以利用WINDOW_NORMAL控制圖片縮放
-"""
+if __name__=='__main__':
+#    thresh1()
+#    thresh2()
+    thresh3()
 
-cv2.namedWindow('im', cv2.WINDOW_NORMAL)
-cv2.imshow('im', im)
+#im = cv2.imread('test.jpg')
+##plt.hist(im.ravel(),256,[0,256]) # 利用直方圖可檢視圖片的明度藉此找到閾值
+#
+## 由於opencv只能處裡灰度圖，所以要先用convert成灰階
+#imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+#ret,thresh = cv2.threshold(imgray,200,255,0) # 經測試200為合適閾值
+##plt.hist(imgray.ravel(),256,[0,256])
+#GaussianBlur = cv2.GaussianBlur(thresh, (9, 9), 0)
+#binary,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#hull = [cv2.convexHull(contours[i]) for i in range(len(contours))]
+#"""
+#cv2.CHAIN_APPROX_NONE,所有的邊界點都會被存儲
+#cv2.CHAIN_APPROX_SIMPLE,只存取重要特徵點
+#"""
+#cv2.drawContours(im,hull,-1,(255, 0, 0), 2)
+#"""
+#我們可以利用 type和 shape來查看圖片的型態和大小
+#print(type(im), im.shape)
+#這裡輸出的結果為    # <class 'numpy.ndarray'>, (3200, 2400, 3)
+#實在是太大了，因此我們可以利用WINDOW_NORMAL控制圖片縮放
+#"""
+#
+#cv2.namedWindow('im', cv2.WINDOW_NORMAL)
+#cv2.imshow('im', im)
+#cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
+#cv2.imshow('thresh', thresh)
 
 # 也許需要利用凸包
 #http://arbu00.blogspot.tw/2016/12/opencv11-contours-and-convex-hull.html
