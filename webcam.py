eb9f729267605f3c737f5f95fb0b50f7a72abc4d
wei <t104306033@ntut.org.tw>
2018-04-30 22:36:06 +0800

    /webcam: test 2 camara

--- webcam.py
+++ webcam.py
@@ -10,10 +10,10 @@ import cv2
 import matplotlib.pyplot as plt
 #import numpy as np
         
-def print_picture(image):
-    cv2.imshow('picture', image)
-def save_picture(image):
-    cv2.imwrite('test.png', image)
+def print_picture(name, image):
+    cv2.imshow('picture_{}'.format(name), image)
+def save_picture(name, image):
+    cv2.imwrite('pic_{}.png'.format(name), image)
 
 class Camera(cv2.VideoCapture):
     def __init__(self, camera_number):
@@ -23,9 +23,9 @@ class Camera(cv2.VideoCapture):
     def preview(self):
         while True:
             ret, img = self.read()
-            img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#            img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             cv2.imshow('preview', img)
-            cv2.imshow('gray', img1)
+#            cv2.imshow('gray', img1)
             self.key = cv2.waitKey(10) # & 0xff
             ''' waitKey([, delay]) -> retval
             delay：等待時間，
@@ -45,21 +45,7 @@ class Camera(cv2.VideoCapture):
                     save_picture(img)
         self.release()
         cv2.destroyAllWindows()
-    def preview_matplotlib(self):
 
-        count = 0
-        """
-        because setting time of the camara too slow,
-        if use the other camara just type "if self.isOpened():"
-        """
-        while count <= 10:
-            count += 1
-            ret, img = self.read()
-        #    print(ret, img)
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            plt.imshow(img)
-            plt.show()
-        self.release()
     def take_picture(self, use_matplotlib=False):
         count = 0
         """
@@ -90,12 +76,28 @@ class Camera(cv2.VideoCapture):
 #        self._cap.release()
     
 if __name__=='__main__':
-    cam = Camera(1)
-    cam.take_picture(True)
+    import cv2
 
-    
-#    get_vision(0)#, get_vision(1)
-#    take_pic(0)
+    cam = [cv2.VideoCapture(i) for i in range(2)]
+#    cam1 = cv2.VideoCapture(0)
+#    cam2 = cv2.VideoCapture(1)
+    ret = [i for i in range(2)]
+    img = [i for i in range(2)]
+    while True:
+        for i in range(2):    
+            ret[i], img[i] = cam[i].read()
+            cv2.imshow(str(i), img[i])
+        print(cv2.waitKey(5))
+        if cv2.waitKey(5) == 27:
+            break
+        if cv2.waitKey(5) == 115:
+            for i in range(2):
+                print_picture(str(i), img[i])
+                save_picture(i, img[i])
+    for i in range(2):
+        cam[i].release()
+    cv2.destroyAllWindows()
+
     
 
 
