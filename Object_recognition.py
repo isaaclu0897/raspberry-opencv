d7335331bec0f54976dc36b96e3cf89403b8caba
wei <t104306033@ntut.org.tw>
2018-05-13 21:59:19 +0800

    /object_recognize: update

--- Object_recognition.py
+++ Object_recognition.py
@@ -10,42 +10,35 @@ import cv2
 import numpy as np
 MIN_MATCH_COUNT = 4
 
-
 # load data
-img1 = cv2.imread('./test_recognition/1371867.jpg')
-img2 = cv2.imread('./test_recognition/1371866.jpg')
+img1 = cv2.imread('./testtt/7938976811261.jpg')
+img2 = cv2.imread('./testtt/7938976997433.jpg')
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 # Create features of detector(SIFT) 
 sift = cv2.xfeatures2d.SIFT_create() # you can choose SIFT or SURF
 
-#(kps, descs) = sift.detectAndCompute(gray1, None)
-#print("# kps: {}, descriptors: {}".format(len(kps), descs.shape))
-## kps: 274, descriptors: (274, 128)
-#surf = cv2.xfeatures2d.SURF_create()
-#(kps, descs) = surf.detectAndCompute(gray1, None)
-#print("# kps: {}, descriptors: {}".format(len(kps), descs.shape))
-## kps: 393, descriptors: (393, 64)
 
-## (3) Create flann matcher
+# Create flann matcher
 matcher = cv2.FlannBasedMatcher(dict(algorithm = 1, trees = 5), {})
 
-## (4) Detect keypoints and compute keypointer descriptors
+# Detect keypoints and compute keypointer descriptors
 kpts1, descs1 = sift.detectAndCompute(gray1,None)
 kpts2, descs2 = sift.detectAndCompute(gray2,None)
 
-## (5) knnMatch to get Top2
+# knnMatch to get Top2
 matches = matcher.knnMatch(descs1, descs2, 2)
-# Sort by their distance.
+
+# $ Sort by their distance.
 matches = sorted(matches, key = lambda x:x[0].distance)
 
-## (6) Ratio test, to get good matches.
+# to get good matches.
 good = [m1 for (m1, m2) in matches if m1.distance < 0.7 * m2.distance]
 
 canvas = img2.copy()
 
-## (7) find homography matrix
+# find homography matrix
 ## 当有足够的健壮匹配点对（至少4个）时
 if len(good)>MIN_MATCH_COUNT:
     ## 从匹配中提取出对应点对
@@ -65,44 +58,26 @@ if len(good)>MIN_MATCH_COUNT:
 else:
     print( "Not enough matches are found - {}/{}".format(len(good),MIN_MATCH_COUNT))
 
+cv2.namedWindow('kkk', cv2.WINDOW_NORMAL)
+cv2.imshow('kkk', canvas)
+# drawMatches
+#draw_params = dict(matchColor = (0,255,0),
+#    singlePointColor = (255,0,0),
+##    matchesMask =1 matchesMask,
+#    flags = 0)
+#matched = cv2.drawMatches(img1,kpts1,img2,kpts2,good,None)#,**draw_params)
 
-## (8) drawMatches
-matched = cv2.drawMatches(img1,kpts1,canvas,kpts2,good,None)#,**draw_params)
+# Crop the matched region from scene
+#h,w = img1.shape[:2]
+#pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+#dst = cv2.perspectiveTransform(pts,M)
+#perspectiveM = cv2.getPerspectiveTransform(np.float32(dst),pts)
+#found = cv2.warpPerspective(img2,perspectiveM,(w,h))
 
-## (9) Crop the matched region from scene
-h,w = img1.shape[:2]
-pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-dst = cv2.perspectiveTransform(pts,M)
-perspectiveM = cv2.getPerspectiveTransform(np.float32(dst),pts)
-found = cv2.warpPerspective(img2,perspectiveM,(w,h))
 
 ## (10) save and display
-cv2.imwrite("matched.png", matched)
-cv2.imwrite("found.png", found)
-cv2.imshow("matched", matched);
-cv2.imshow("found", found);
-cv2.waitKey();cv2.destroyAllWindows()
-
-#%%
-import cv2
-import numpy as np
-
-def SIFT(img):
-    I = cv2.imread(img)
-    gray = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
-    descriptor = cv2.xfeatures2d.SIFT_create()
-    kps, features = descriptor.detectAndCompute(gray, None)
-    nkps = []
-    for i in kps:
-        if i.size > 100:
-            nkps.append(i)
-    cv2.drawKeypoints(I,nkps,I,(0,255,255),flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow('Vikings Detected!!', I)
-    cv2.imwrite('sift_keypoints.jpg',I)
-    cv2.waitKey(0)
-
-if __name__ == '__main__':
-    img = './test_recognition/1371867.jpg'
-    SIFT(img)
-    
-# https://blog.csdn.net/yzxnuaa/article/details/79221174
+#cv2.imwrite("matched.png", matched)
+#cv2.imwrite("found.png", found)
+#cv2.imshow("matched", matched);
+#cv2.imshow("found", found);
+#cv2.waitKey();cv2.destroyAllWindows()
